cd549de40eaf1e461ba629a18a980e200cc4b2bf
zsu-v3 <207527508+zsu-v3@users.noreply.github.com>
2025-07-03 15:30:14 +0200

    Update the nodelist check in configuration

--- src/ui/composables/useValidators.js
+++ src/ui/composables/useValidators.js
@@ -20,7 +20,9 @@ export default function useValidators() {
             return /^[^\0]+$/.test(v || "");
         },
         commaSeparatedList(v) {
-            return /^([a-zA-Z0-9_-]*)(,[a-zA-Z0-9_-]*)*$/.test(v || "");
+            return /^([a-zA-Z0-9_\-\[\]]*)(,[a-zA-Z0-9_\-\[\]]*)*$/.test(
+                v || ""
+            );
         },
         noSpaces(v) {
             return !/\s/g.test(v || "");
