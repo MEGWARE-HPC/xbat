da4da0255360dba1148c859dcd5f32eaaded5c5f
zsu-v3 <207527508+zsu-v3@users.noreply.github.com>
2025-08-07 12:01:17 +0200

    Try 17.2

--- docs/components/content/ActionButton.vue
+++ docs/components/content/ActionButton.vue
@@ -5,14 +5,25 @@
             class="btn"
             :append-icon="props.icon"
             :href="props.href"
+            :target="props.target"
+            rel="noopener noreferrer"
             ><ContentSlot :use="$slots.default"
         /></v-btn>
     </div>
 </template>
 <script setup lang="ts">
 const props = withDefaults(
-    defineProps<{ color: string; icon: string | undefined; href: string }>(),
-    { color: "primary", href: "#" }
+    defineProps<{
+        color?: string;
+        icon?: string;
+        href: string;
+        target?: string;
+    }>(),
+    {
+        color: "primary",
+        href: "#",
+        target: "_self"
+    }
 );
 </script>
 <style scoped lang="scss">
