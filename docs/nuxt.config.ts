d67fb13fd9bf12d538c06162d580db428748b54c
zsu-v3 <207527508+zsu-v3@users.noreply.github.com>
2026-01-07 09:01:22 +0100

    Optimize Nuxt config

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -23,26 +23,6 @@ export default defineNuxtConfig({
         "@pinia/nuxt",
         "@nuxtjs/sitemap"
     ],
-    fonts: {
-        families: [
-            {
-                name: "Source Sans 3",
-                provider: "google",
-                weights: [400, 500, 600],
-                styles: ["normal", "italic"],
-                subsets: ["latin"],
-                display: "swap"
-            },
-            {
-                name: "Source Code Pro",
-                provider: "google",
-                weights: [400],
-                styles: ["normal"],
-                subsets: ["latin"],
-                display: "swap"
-            }
-        ]
-    },
     nitro: {
         prerender: {
             routes: [...routes]
